6da006899cb56bdbcc853e38d30550c33dd6f829
evlazarev007 <115842128+evlazarev007@users.noreply.github.com>
2022-12-13 00:56:26 +0600

    Add code

--- loops1.sh
+++ loops1.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-# Put your code here
+input="$1"
+output=""
+
+for ((i=${#input};i>=0;i--)); do
+	char="${input:$i:1}"
+	if [[ $char =~ [A-Z] ]]; then
+		output+="${char,,}"
+	elif [[ $char =~ [a-z] ]]; then
+		output+="${char^^}"
+	else output+="$char"
+	fi
+done
+
+echo "$output"
